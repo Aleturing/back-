00b51c38afc6e78548f209fb16ed21d1532a76fd
Aleturing <enriquedell00@gmail.com>
2025-04-28 22:28:56 -0400

    update

--- database/db.js
+++ database/db.js
@@ -1,20 +1,18 @@
 const { Client } = require('pg');
-const path = require('path');
 
-// Configura la conexión con PostgreSQL (ajusta los valores según tu configuración)
+// La URL de conexión a PostgreSQL proporcionada por Supabase
+const supabaseConnectionUrl = 'postgresql://postgres:Fallout#123456789Metro@db.sytbqsmmmeetawleiktx.supabase.co:5432/postgres';
+
+// Configura la conexión con PostgreSQL (ajusta la URL según tu configuración de Supabase)
 const client = new Client({
-    user: 'tu_usuario',
-    host: 'localhost', // o tu host de base de datos
-    database: 'papeleria', // nombre de la base de datos
-    password: 'tu_contraseña',
-    port: 5432,
+    connectionString: supabaseConnectionUrl,
 });
 
 client.connect((err) => {
     if (err) {
         console.error('Error al conectar a la base de datos PostgreSQL:', err.message);
     } else {
-        console.log('Conexión exitosa a la base de datos PostgreSQL.');
+        console.log('Conexión exitosa a la base de datos PostgreSQL en Supabase.');
         // Listar tablas en la base de datos
         client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public';", (err, res) => {
             if (err) {
