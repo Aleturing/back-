19f8fbd5c519d7c27d02be43d69934520f176546
Aleturing <enriquedell00@gmail.com>
2025-04-28 22:35:00 -0400

    update

--- database/db.js
+++ database/db.js
@@ -1,9 +1,16 @@
+// La contraseña con caracteres especiales
+const password = 'Fallout#123456789Metro';
+
+// Codificamos la contraseña
+const encodedPassword = encodeURIComponent(password);
+
+// Construimos la URL de conexión
+const supabaseConnectionUrl = `postgresql://postgres:${encodedPassword}@db.sytbqsmmmeetawleiktx.supabase.co:5432/postgres`;
+
+
+
+// Luego, puedes usar esta URL en tu cliente de conexión
 const { Client } = require('pg');
-
-// La URL de conexión a PostgreSQL proporcionada por Supabase
-const supabaseConnectionUrl = 'postgresql://postgres:[Fallout#123456789Metro]@db.sytbqsmmmeetawleiktx.supabase.co:5432/postgres';
-
-// Configura la conexión con PostgreSQL (ajusta la URL según tu configuración de Supabase)
 const client = new Client({
     connectionString: supabaseConnectionUrl,
 });
@@ -13,14 +20,12 @@ client.connect((err) => {
         console.error('Error al conectar a la base de datos PostgreSQL:', err.message);
     } else {
         console.log('Conexión exitosa a la base de datos PostgreSQL en Supabase.');
-        // Listar tablas en la base de datos
         client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public';", (err, res) => {
             if (err) {
                 console.error('Error al listar tablas:', err.message);
             } else {
                 console.log('Tablas existentes en la base de datos:', res.rows);
             }
-            // Cierra la conexión después de realizar la consulta
             client.end();
         });
     }
