98a77984e072971956ad85ed8492d30c32b99410
Aleturing <enriquedell00@gmail.com>
2025-05-04 21:55:31 -0400

    local

--- models/Producto.js
+++ models/Producto.js
@@ -1,55 +1,30 @@
+// models/Producto.js
 const db = require("../database/db");
 
 const Producto = {
-  getAll: (callback) => {
-    const sql = "SELECT * FROM productos";
-    db.query(sql, (err, results) => {
-      callback(err, results);
-    });
-  },
-
-  getById: (id, callback) => {
-    const sql = "SELECT * FROM productos WHERE id = ?";
-    db.query(sql, [id], (err, results) => {
-      callback(err, results[0]);
-    });
-  },
-
-  create: (data, callback) => {
-    const { nombre, descripcion, precio, stock, foto } = data;
-    const sql = `
-      INSERT INTO productos (nombre, descripcion, precio, stock, foto)
-      VALUES (?, ?, ?, ?, ?)
-    `;
-    db.query(
-      sql,
-      [nombre, descripcion, precio, stock, foto],
-      (err, results) => {
-        callback(err, results.insertId);
-      }
-    );
-  },
-
-  update: (id, data, callback) => {
-    const { nombre, descripcion, precio, stock, foto } = data;
-    const sql = `
-      UPDATE productos
-      SET nombre = ?, descripcion = ?, precio = ?, stock = ?, foto = ?
-      WHERE id = ?
-    `;
-    db.query(
-      sql,
-      [nombre, descripcion, precio, stock, foto, id],
-      (err, results) => {
-        callback(err, results.affectedRows);
-      }
-    );
-  },
+  // ... otros métodos ...
 
   delete: (id, callback) => {
-    const sql = "DELETE FROM productos WHERE id = ?";
-    db.query(sql, [id], (err, results) => {
-      callback(err, results.affectedRows);
+    // Eliminar registros en detalle_factura
+    const sqlDetalleFactura = "DELETE FROM detalle_factura WHERE producto_id = ?";
+    db.query(sqlDetalleFactura, [id], (err) => {
+      if (err) return callback(err);
+
+      // Eliminar registros en detalle_devolucion
+      const sqlDetalleDevolucion = "DELETE FROM detalle_devolucion WHERE producto_id = ?";
+      db.query(sqlDetalleDevolucion, [id], (err) => {
+        if (err) return callback(err);
+
+        // Finalmente, eliminar el producto
+        const sqlProducto = "DELETE FROM productos WHERE id = ?";
+        db.query(sqlProducto, [id], (err, results) => {
+          if (err) return callback(err);
+          const deleted = results && typeof results.affectedRows === "number"
+            ? results.affectedRows
+            : 0;
+          callback(null, deleted);
+        });
+      });
     });
   }
 };
